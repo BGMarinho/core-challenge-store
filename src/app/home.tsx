9197fcb126b1dc851dc77ff6856a0185f5da7666
BGMarinho <bgmarinho96@gmail.com>
2024-12-10 22:16:28 -0300

    chore (setup): Cleaning some useless stuff and starting some structure on home.

---
+++ src/app/home.tsx
@@ -0,0 +1,12 @@
+import Image from 'next/image';
+import styled from 'styled-components';
+
+export default function Home() {
+  return (
+    <div>
+      <header></header>
+      <main></main>
+      <footer></footer>
+    </div>
+  );
+}
