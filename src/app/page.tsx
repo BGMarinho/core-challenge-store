6e63d57a03ecbe71bac8ff1dc91824d8cf056b05
BGMarinho <bgmarinho96@gmail.com>
2024-12-10 23:18:33 -0300

    feature (structures and images): Putting the first styled main components in a satisfatory position and addin some png and svg images.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,13 +3,16 @@ import Image from 'next/image';
 import styled from 'styled-components';
 
 const Container = styled.div``;
+const Header = styled.header``;
+const Main = styled.main``;
+const Footer = styled.footer``;
 
 export default function Home() {
   return (
     <Container>
-      <header></header>
-      <main></main>
-      <footer></footer>
+      <Header></Header>
+      <Main></Main>
+      <Footer></Footer>
     </Container>
   );
 }
