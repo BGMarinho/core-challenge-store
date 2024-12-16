992ca53ef40f1d2e19f1da1992f029cb163aa753
BGMarinho <bgmarinho96@gmail.com>
2024-12-15 23:47:58 -0300

    chore (project): Configurin the project to start coding.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,8 +14,8 @@ const geistMono = localFont({
 });
 
 export const metadata: Metadata = {
-  title: 'Corebiz | Tecnical Challenge',
-  description: '',
+  title: 'Corebiz | Technical Challenge',
+  description: 'Front-end',
 };
 
 export default function RootLayout({
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,42 @@
 'use client';
-import Image from 'next/image';
+// import corebizWhite from '../../public/png/site-logo-corebiz-branco.png';
+// import email from '../../public/svg/email.svg';
+// import headset from '../../public/svg/headset.svg';
+import Header from '@/components/Header';
+import MainShelf from '@/components/MainShelf';
 import styled from 'styled-components';
 
-const Container = styled.div``;
-const Header = styled.header``;
-const Main = styled.main``;
-const Footer = styled.footer``;
+const Container = styled.div`
+  display: grid;
+  grid-template-columns: auto;
+  grid-template-rows: auto auto auto auto;
+  grid-template-areas: 'Header' 'Banner' 'Main' 'Newsletter' 'Footer';
+  padding: 0rem 1.5rem;
+  gap: 1rem;
+`;
+const Banner = styled.div`
+  grid-area: Banner;
+`;
+const Newsletter = styled.section`
+  grid-area: Newsletter;
+  display: flex;
+  background-color: #f2f2f2;
+  color: #000000;
+`;
+const Footer = styled.footer`
+  grid-area: Footer;
+  display: flex;
+  background-color: #000000;
+  color: #ffffff;
+`;
 
 export default function Home() {
   return (
     <Container>
-      <Header></Header>
-      <Main></Main>
+      <Header />
+      <Banner></Banner>
+      <MainShelf />
+      <Newsletter></Newsletter>
       <Footer></Footer>
     </Container>
   );
